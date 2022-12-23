7ff3d5ab45a27d78abc8d81820a773c269431483
Slipp Douglas Thompson <git@capnslipp.com>
2022-12-22 22:41:28 -0600

    Initial `doWhileDo(…)` & `doUntilDo(…)` implementations
    * Created initial basic `doWhileDo(_ do1:, while:, do do2:)` implementation.
    	* Uses `DoFunc` & `WhileConditionFunc` closure typealiases.
    * Created initial basic `doUntilDo(_ do1:, until:, do do2:)` implementation (which is `doWhileDo(…)` with the condition inverted).
    	* Uses `DoFunc` & `UntilConditionFunc` closure typealiases.

--- Sources/doWhileDo/doWhileDo.swift
+++ Sources/doWhileDo/doWhileDo.swift
@@ -1,6 +1,43 @@
-public struct doWhileDo {
-    public private(set) var text = "Hello, World!"
+// doWhileDo
+// @author: Slipp Douglas Thompson
+// @license: Public Domain per The Unlicense.  See accompanying LICENSE file or <http://unlicense.org/>.
 
-    public init() {
-    }
+
+
+// MARK: Types
+
+public typealias DoFunc = ()->Void
+public typealias WhileConditionFunc = ()->Bool
+public typealias UntilConditionFunc = ()->Bool
+
+
+
+// MARK: do-while-do
+
+@inlinable
+public func doWhileDo(_ do1: DoFunc, while: WhileConditionFunc, do do2: DoFunc)
+{
+	while true {
+		do1()
+		guard `while`() else {
+			return
+		}
+		do2()
+	}
+}
+
+
+
+// MARK: do-until-do
+
+@inlinable
+public func doUntilDo(_ do1: DoFunc, until: UntilConditionFunc, do do2: DoFunc)
+{
+	while true {
+		do1()
+		guard !until() else {
+			return
+		}
+		do2()
+	}
 }
